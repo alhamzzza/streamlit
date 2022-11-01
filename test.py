bf6c3e39933086e6ca51ee024d4690ca7e364820
alhamzzza <61141101+alhamzzza@users.noreply.github.com>
2022-11-01 14:41:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pandas as pd
-import base64
+
 
 
 
 st.title('Beykoz calculator')
 
 
-x = st.number_input("number of people" , key="x" , value=1)
+buff, col, buff2 = st.columns([1,3,1])
+
+
+
+
+x = col.number_input("number of people" , key="x" , value=1 , step=0)
 d = {}
 for i in range(x):
-    d[f"person{i}"] =  st.number_input(f"person number {int(i)}",  value=0,key=f"{i} ")
+    d[f"person{i}"] =  col.number_input(f"person number {int(i)}",  value=0,key=f"{i} " , step=5)
     #d[st.text_input(label=" ", value=f"person number {int(i)}" )] = st.number_input("",  value=0,key=f"{i} ")
   
 
@@ -21,18 +26,18 @@ for i in range(x):
 df = pd.DataFrame({ "name":[k for k,v in d.items()],"orignal price":[v for k,v in d.items()],"indirmli":[v - v*0.25 for k,v in d.items()]})
 
 
-if x != 1:
+if  list(d.values())[0] !=0:
 
-    st.write(pd.DataFrame({"total price":[df[df.columns[1]].sum()],"indirmli":[df[df.columns[2]].sum()]}))
-
-
-    st.write(df)
+    col.write(pd.DataFrame({"total price":[df[df.columns[1]].sum()],"indirmli":[df[df.columns[2]].sum()]}))
 
 
-    agree = st.checkbox('show chart')
+    col.write(df)
+
+
+    agree = col.checkbox('show chart')
 
     if agree:
-        st.bar_chart(df[df.columns[1]])
+        col.bar_chart(df[df.columns[1]])
 
 
 import base64
@@ -53,3 +58,9 @@ def add_bg_from_url():
      )
 
 add_bg_from_url()
+
+m = st.markdown(""" <style> div.stCheckbox > checkbox { 
+                    background-color: rgb(204, 49, 49); } 
+                    </style>""", unsafe_allow_html=True)
+
+b = st.checkbox("ghj")
